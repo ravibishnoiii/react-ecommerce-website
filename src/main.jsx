e045ba90829d7a6d2179b1987fcd11520f6dd06c
ravibishnoiii <ravikumargodara114@gmail.com>
2024-04-18 09:13:54 +0530

    changes made in package.json,main and viteconfig

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Context from './utils/Context.jsx'
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Context>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <ToastContainer />
-    </BrowserRouter>
+    </HashRouter>
   </Context>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/react-ecommerce-website/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./src/main.jsx",
-      },
-    },
-  },
+  
 });
